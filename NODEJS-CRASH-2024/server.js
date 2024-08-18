787ab5457ff0e8b1cc1d003cb19dc50ecae7d6d9
ObedNdayizera <obedndayizera@gmail.com>
2024-08-18 22:08:45 +0200

    push changes

--- NODEJS-CRASH-2024/server.js
+++ NODEJS-CRASH-2024/server.js
@@ -1,6 +1,13 @@
 import http from 'http';
 import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// console.log(__filename, __dirname);
 
 const server = http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'text/html');
