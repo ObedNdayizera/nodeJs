5799e2a6d44e7caa7addc4775b8d77dc1db293a3
ObedNdayizera <obedndayizera@gmail.com>
2024-08-23 22:49:31 +0200

    push changes

--- NODEJS-CRASH-2024/server2.js
+++ NODEJS-CRASH-2024/server2.js
@@ -23,14 +23,14 @@ const jsonMiddleware = (req, res, next) => {
 }
 
 
-//Get Users Handle
+//Route Handle For Get /api/users
 const getUsersHandle = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.write(JSON.stringify(users));
     res.end();
 }
 
-// Get User By Id Handle
+// Route Handle For Get By Id /api/users/:id
 const getUserByIdHandle = (req, res) => {
     const id = req.url.split('/')[3];
     const user = users.find((user) => user.id === parseInt(id));
@@ -45,9 +45,27 @@ const getUserByIdHandle = (req, res) => {
 }
 
 
+//Route Handle For Post /api/users
+const createUserHandle = (req, res) => {
+    let body = '';
+    // Listen For Data
+    req.on('data', (chunk) => {
+        body += chunk.toString();
+    });
+
+    req.on('end', () => {
+        let newUser = JSON.parse(body);
+        users.push(newUser);
+        res.statusCode = 201;
+        res.write(JSON.stringify(newUser));
+        res.end();
+    })
+}
+
+
 // Not Found Handle
 const notFoundHandle = (req, res) => {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.statusCode = 404;
     res.write(JSON.stringify({ message: "Route not found" }));
     res.end();
 }
@@ -60,6 +78,8 @@ const server = createServer((req, res) => {
                 getUsersHandle(req, res);
             } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
                 getUserByIdHandle(req, res);
+            } else if (req.url === '/api/users' && req.method === "POST") {
+                createUserHandle(req, res);
             } else {
                 notFoundHandle(req, res);
             }
