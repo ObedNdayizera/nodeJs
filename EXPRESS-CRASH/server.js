0786fc4938c8a80b0a5a4049730e8bc166911067
ObedNdayizera <obedndayizera@gmail.com>
2024-09-08 22:41:30 +0200

    changes to es module and cleaned up codes

--- EXPRESS-CRASH/server.js
+++ EXPRESS-CRASH/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const posts = require('./routes/posts')
 
 const app = express();
 
@@ -8,35 +9,8 @@ const PORT = process.env.PORT || 8000;
 //setup static folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
-let posts = [
-    { id: 1, title: "Post One" },
-    { id: 2, title: "Post Two" },
-    { id: 3, title: "Post Three" }
-]
+app.use('/api/posts', posts)
 
-//Get all posts
-app.get('/api/posts', (req, res) => {
-    let limit = parseInt(req.query.limit);
-
-    if (!isNaN(limit) && limit > 0) {
-        res.status(200).json(posts.slice(0, limit));
-    } else {
-        res.status(200).json(posts);
-    }
-})
-
-//Get a single post
-app.get('/api/posts/:id', (req, res) => {
-    let id = parseInt(req.params.id);
-    let post = posts.find(post => post.id == id);
-
-    if (!post) {
-        res.status(404).json({ message: `A Post With Id Of ${id} Was Not Found` });
-    } else {
-        res.status(200).json(post);
-    }
-
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
