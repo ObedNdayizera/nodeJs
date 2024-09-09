a7ee23385ff3b49474962f4ea88a191a75c4beb6
ObedNdayizera <obedndayizera@gmail.com>
2024-09-09 21:13:46 +0200

    update in express js

--- EXPRESS-CRASH/routes/posts.js
+++ EXPRESS-CRASH/routes/posts.js
@@ -31,4 +31,53 @@ router.get('/:id', (req, res) => {
 
 })
 
+//Create New Post
+router.post('/', (req, res) => {
+    const newPost = {
+        id: posts.length + 1,
+        title: req.body.title
+    }
+
+    if (!newPost.title) {
+        return res.status(400).json({ msg: "Please Include A Title" });
+    }
+
+    posts.push(newPost);
+    res.status(201).json(posts);
+})
+
+//Update post
+router.put('/:id', (req, res) => {
+    // let id = parseInt(req.params.id);
+    // let index = posts.findIndex(p => id == p.id);
+
+    // if (index == -1) {
+    //     return res.status(201).json({ msg: "Please specific right post" });
+    // }
+
+    // let updPost = {
+    //     id,
+    //     title: req.body.title || posts[index].title
+    // }
+
+    // posts[index] = updPost;
+
+    // res.status(201).json(posts);
+
+
+
+
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        const error = new Error(`A post with the id of ${id} was not found`);
+        error.status = 404;
+        return next(error);
+    }
+
+    post.title = req.body.title;
+    res.status(200).json(posts);
+})
+
 export default router;
--- EXPRESS-CRASH/server.js
+++ EXPRESS-CRASH/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import path from 'path';
-import posts from './routes/posts';
+import posts from './routes/posts.js';
+
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 //setup static folder
 // app.use(express.static(path.join(__dirname, 'public')));
